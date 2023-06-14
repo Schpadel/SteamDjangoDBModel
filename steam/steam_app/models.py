9b69eca3286a5fa71a0cdfb26c7da735272cd207
Patrick Sandmann <sandmann.patrick@yahoo.de>
2023-06-14 12:44:23 +0200

    working project settings

--- steam/steam_app/models.py
+++ steam/steam_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
 class Game(models.Model):
     name = models.CharField(max_length=200)
     release_date = models.DateTimeField("date released")
-
--- steam/steam_app/tests.py
+++ steam/steam_app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Game
+
 
 # Create your tests here.
+class GameTestCase(TestCase):
+    def setUp(self):
+        Game.objects.create(name='DiabloIV', release_date='01.01.2000')
