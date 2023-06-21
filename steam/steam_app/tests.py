e6ebee007d4f35e95d51e8fa759d219fc0502ad0
Patrick Sandmann <sandmann.patrick@yahoo.de>
2023-06-21 12:52:41 +0200

    fixtues skeleton

--- steam/steam_app/tests.py
+++ steam/steam_app/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Game
+from .models import *
 
 ALL_FIXTURES = ["games.json"]
 
@@ -11,3 +11,8 @@ class GameTestCase(TestCase):
     def test_create_game(self):
         diablo = Game.objects.get(pk=1)
         self.assertEquals(diablo.name, "Diablo IV")
+
+    def test_create_user(self):
+        user = User.objects.get(pk=1)
+        self.assertEquals(user.firstname, "Schpadel")
+
