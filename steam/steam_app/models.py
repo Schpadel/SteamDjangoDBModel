12762c791e4392e3ab2dae50a3ac6870697617e9
Patrick Sandmann <sandmann.patrick@yahoo.de>
2023-06-21 12:35:00 +0200

    first working test

--- steam/steam_app/models.py
+++ steam/steam_app/models.py
@@ -3,28 +3,33 @@ from django.db import models
 
 # Create your models here.
 class Game(models.Model):
-    name = models.CharField(max_length=200, default="Undefined")
+    name = models.CharField(max_length=200)
     genre = models.CharField(max_length=200)
-    publisher = models.CharField(max_length=200, default="Undefined")
-    developer = models.CharField(max_length=200, default="Undefined")
-    franchise = models.CharField(max_length=200, null=True)
+    publisher = models.CharField(max_length=200)
+    developer = models.CharField(max_length=200)
+    franchise = models.CharField(max_length=200)
     price = models.DecimalField(decimal_places=2, max_digits=10)
     size = models.DecimalField(decimal_places=2, max_digits=100)
     rating = models.SmallIntegerField()
-    release_date = models.DateTimeField("date released")
+    release_date = models.DateField("date released")
     usk = models.SmallIntegerField()
     supported_platforms = models.CharField(max_length=200, default="PC")
-    supported_languages = models.CharField(max_length=200, default="EN")
-    description = models.CharField(max_length=20_000, null=True)
+    supported_languages = models.CharField(max_length=200)
+    description = models.CharField(max_length=20_000)
 
     class Meta:
         constraints = [models.CheckConstraint(name="Test Int Constraint", check=models.Q(rating__range=(0, 100)), )]
 
 
 class Library(models.Model):
-    timePlayed = models.DateTimeField()
+    timePlayed = models.DurationField()
     lastPlayed = models.DateTimeField()
     cloudSaveStatus = models.BooleanField()
+    game = models.ManyToManyField(Game)
+
+
+class Wishlist(models.Model):
+    gameID = models.ManyToManyField(Game)
 
 
 class User(models.Model):
@@ -32,30 +37,28 @@ class User(models.Model):
     address = models.CharField(max_length=200)
     firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200)
-    birthday = models.DateTimeField()
+    birthday = models.DateField()
     vac = models.BooleanField()
     level = models.IntegerField()
-    library_id = models.OneToOneField(Library, on_delete=models.CASCADE)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    wishList = models.OneToOneField(Wishlist, on_delete=models.CASCADE)
 
 
 class Achievement(models.Model):
     name = models.CharField(max_length=200, default="Undefined")
     description = models.CharField(max_length=200, null=True)
-    gameID = models.IntegerField()
-
-
-class Wishlist(models.Model):
-    gameID = models.IntegerField()
+    gameID = models.ForeignKey(Game, on_delete=models.CASCADE)
 
 
 class Review(models.Model):
     heading = models.CharField(max_length=200, default="Undefined")
     text = models.CharField(max_length=200, null=True)
     rating = models.SmallIntegerField()
-    userID = models.IntegerField()
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    gameID = models.ForeignKey(Game, on_delete=models.CASCADE)
 
 
 class AchievedBy(models.Model):
-    userID = models.CharField(max_length=200)
-    achievementID = models.IntegerField()
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    achievementID = models.ForeignKey(Achievement, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
--- steam/steam_app/tests.py
+++ steam/steam_app/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 from .models import Game
 
+ALL_FIXTURES = ["games.json"]
+
 
 # Create your tests here.
 class GameTestCase(TestCase):
-    def setUp(self):
-        Game.objects.create(name='DiabloIV', release_date='01.01.2000')
+    fixtures = ALL_FIXTURES
+
+    def test_create_game(self):
+        diablo = Game.objects.get(pk=1)
+        self.assertEquals(diablo.name, "Diablo IV")
