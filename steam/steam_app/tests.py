657d779f285bd7194d3cff028d9e3eb964006924
Filippo Fiorenza <filippo1998@gmx.de>
2023-06-23 13:58:46 +0200

    first test for game + added fixtures

--- steam/steam_app/tests.py
+++ steam/steam_app/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import *
+import json
+
+from .steamImplementation import Steam
 
 ALL_FIXTURES = ["games.json"]
 
@@ -15,4 +18,14 @@ class GameTestCase(TestCase):
     def test_create_user(self):
         user = User.objects.get(pk=1)
         self.assertEquals(user.firstname, "Schpadel")
+
+    def test_publish_new_game(self):
+        game = Game(publisher="Giants Software", size=30, developer="Giants Software", franchise="Simulation",
+                    description="Farm some shit", release_date="2023-05-23", supported_languages="EN",
+                    supported_platforms="PC,Switch", rating=69, usk=0, genre="Simulation", price=44.99,
+                    name="Landwirtschafts Simulator 23")
+        achievements = list()
+        # achievements.append(Achievement(gameID_id=1, ))
+        Steam.release_new_game(game, achievements)
+
 
